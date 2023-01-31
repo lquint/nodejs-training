888d7235eb093281fa780b22454c95b2b114a4b4
lquint <83752971+lquint@users.noreply.github.com>
2023-01-31 17:58:39 +0100

    added get /directories route

--- routes/file.route.js
+++ routes/file.route.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs')
+const { readdir } = require('fs/promises')
+const path = require('path')
+const { resolve } = require('path');
 
 router.get('/', (req,res)=>{
     res.render('pages/files');
 });
+
+router.get('/directories', async(req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    async function getFiles(dir) {
+        const dirents = await readdir(dir, { withFileTypes: true });
+        const files = await Promise.all(dirents.map(async(dirent) => {
+          const res = resolve(dir, dirent.name);
+          return dirent.isDirectory() ? {[dirent.name]: await getFiles(res)} : dirent.name;
+        }));
+        return files.flat();
+      }
+     const files = await getFiles(path.resolve(__dirname, '../views'))
+     res.json({data:files})
+     res.end()
+})
 
 module.exports = router;
