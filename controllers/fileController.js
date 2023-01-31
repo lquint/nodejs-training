9b9a2be47440478c6b47dde818580196ab50186f
lquint <83752971+lquint@users.noreply.github.com>
2023-01-31 22:37:55 +0100

    formated and removed unused imports

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,19 +1,22 @@
-const fs = require('fs')
-const { readdir } = require('fs/promises')
-const path = require('path')
-const { resolve } = require('path');
+const fs = require("node:fs");
+const { readdir } = require("fs/promises");
+const { resolve } = require("path");
 
-exports.getFiles = async(req,res)=>{
-    res.setHeader('Content-Type', 'application/json');
-    async function getFiles(dir) {
-        const dirents = await readdir(dir, { withFileTypes: true });
-        const files = await Promise.all(dirents.map(async(dirent) => {
-          const res = resolve(dir, dirent.name);
-          return dirent.isDirectory() ? {[dirent.name]: await getFiles(res)} : dirent.name;
-        }));
-        return files.flat();
-      }
-     const files = await getFiles(path.resolve(__dirname, '../demoFolder'))
-     res.json({data:files})
-     res.end()
-    }
+exports.getFiles = async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  async function getFiles(dir) {
+    const dirents = await readdir(dir, { withFileTypes: true });
+    const files = await Promise.all(
+      dirents.map(async (dirent) => {
+        const res = resolve(dir, dirent.name);
+        return dirent.isDirectory()
+          ? { [dirent.name]: await getFiles(res) }
+          : dirent.name;
+      })
+    );
+    return files.flat();
+  }
+  const files = await getFiles(resolve(__dirname, "../demoFolder"));
+  res.json({ data: files });
+  res.end();
+};
