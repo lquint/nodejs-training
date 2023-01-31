859a038286ffba39461025ecb2ecb1ced8e654d7
lquint <83752971+lquint@users.noreply.github.com>
2023-01-31 23:23:37 +0100

    formated

--- routes/central.route.js
+++ routes/central.route.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.render('pages/home');
+router.get("/", (req, res) => {
+  res.render("pages/home");
 });
 
 module.exports = router;
--- routes/file.route.js
+++ routes/file.route.js
@@ -1,19 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs')
-const { readdir } = require('fs/promises')
-const path = require('path')
-const { resolve } = require('path');
-const fileController = require('../controllers/fileController')
+const fileController = require("../controllers/fileController");
 
-router.get('/', (req,res)=>{
-    res.render('pages/files');
+router.get("/", (req, res) => {
+  res.render("pages/files");
 });
 
+router.get("/directories", fileController.getFiles);
 
-router.get('/directories',fileController.getFiles )
-
-router.post('/writeFile',fileController.writeFile)
-router.delete('/deleteFile',fileController.deleteFile)
+router.post("/writeFile", fileController.writeFile);
+router.delete("/deleteFile", fileController.deleteFile);
 
 module.exports = router;
