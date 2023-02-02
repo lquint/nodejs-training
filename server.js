2540abbe89afc67b0fa45173f4c15890b50d7ac8
lquint <83752971+lquint@users.noreply.github.com>
2023-02-02 15:53:16 +0100

    using body parser middleware

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const fileRoutes = require('./routes/file.route')
 const centralRoutes= require('./routes/central.route')
+var bodyParser = require('body-parser');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+app.use(express.json())
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/',centralRoutes)
 app.use('/fs',fileRoutes)
